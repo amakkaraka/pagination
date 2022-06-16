7ee4f9079225f14ffd009170c8cbf2ee0b1d0475
amakkaraka <76583382+amakkaraka@users.noreply.github.com>
2022-06-16 09:54:14 +0000

    second commit

---
+++ pagination/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { reactive } from "vue";
+
+const data = reactive({
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+  errors: {},
+  loading: false,
+});
+</script>
+
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 m-auto">
+          <h1>Hallo dunia</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ pagination/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+
+    <paginate
+    :page-count="20"
+    :page-range="3"
+    :margin-pages="2"
+    :click-handler="clickCallback"
+    :prev-text="'Prev'"
+    :next-text="'Next'"
+    :container-class="'pagination'"
+    :page-class="'page-item'"
+  >
+</div>
+</template>
+
+<script setup>
+
+const clickCallback = (page) => {
+  console.log(page)
+}
+</script> 
